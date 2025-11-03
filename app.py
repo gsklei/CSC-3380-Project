7247a59b0dc03dcd2a1880b438befa04855de287
Aeris Kelleher <aeriskelleher@aa-7f-6d-4c-85-08.wlan.lsu.edu>
2025-11-02 18:50:50 -0600

    basic ui

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
-from flask import Flask
+import re
+from datetime import datetime
+
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+
+@app.route('/browse')
+def browse():
+    items = [
+        {
+            'name': 'Blue Plaid Shirt',
+            'category': 'tops',
+            'image_url': '/static/images/shirt.jpg',
+            'tags': ['casual', 'plaid']
+        },
+        # ... more items
+    ]
+    return render_template('browse.html', 
+                         items=items, 
+                         item_count=len(items))
