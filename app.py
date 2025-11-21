14d4c902f8f1cd185b89dbe36c5d070ff29aa36a
Aeris Kelleher <aeriskelleher@Aeriss-MacBook-Pro.local>
2025-11-21 13:53:24 -0600

    fixed app routing and deleted most hardcoding

--- app.py
+++ app.py
@@ -37,13 +37,15 @@ def upload_image():
         file_url = url_for('get_file', filename=filename)
     else:
         file_url= None    
-    return render_template('.html', form=form, file_url=file_url)
+    return render_template('browse.html', form=form, file_url=file_url)
 
 if __name__=='__main__':
     app.run(debug=True)
 
-@app.route("/")
+@app.route('/')
 def browse():
+    form = UploadForm() 
+
     items = [
         {
             "name": "White TOP Shirt",
@@ -66,116 +68,13 @@ def browse():
             "tags": ["casual", "sneaker"],
         },
 
-        {
-            "name": "Psycho?",
-            "category": "tops",
-            "image_url": "/static/images/Screenshot 2025-11-15 020645.png",
-            "tags": ["casual", "outfit", "graphic"],
-        },
-
-        {
-            "name": "Purple Reign Tee",
-            "category": "tops",
-            "image_url": "/static/images/Screenshot 2025-11-15 020712.png",
-            "tags": ["casual", "graphic"],
-        },
-
-        {
-            "name": "Abandonment Hoodie",
-            "category": "tops",
-            "image_url": "/static/images/Screenshot 2025-11-15 020736.png",
-            "tags": ["jacket", "winter", "graphic"],
-        },
-
-        {
-            "name": "Gratitude WWIC Tee",
-            "category": "tops",
-            "image_url": "/static/images/Screenshot 2025-11-15 020801.png",
-            "tags": ["casual", "graphic"],
-        },
-
-        {
-            "name": "Wright Brothers Tee",
-            "category": "tops",
-            "image_url": "/static/images/Screenshot 2025-11-15 020814.png",
-            "tags": ["casual", "outfit"],
-        },
-
-        {
-            "name": "Off WWIC Tee",
-            "category": "tops",
-            "image_url": "/static/images/Screenshot 2025-11-15 020820.png",
-            "tags": ["custom", "outfit"],
-        },
-
-        {
-            "name": "LUCKI Tee",
-            "category": "tops",
-            "image_url": "/static/images/Screenshot 2025-11-15 020848.png",
-            "tags": ["custom", "outfit"],
-        },
-
-        {
-            "name": "unLUCKI Tee",
-            "category": "tops",
-            "image_url": "/static/images/Screenshot 2025-11-15 020853.png",
-            "tags": ["custom", "outfit"],
-        },
-
-        {
-            "name": "Simone Green Tee",
-            "category": "tops",
-            "image_url": "/static/images/Screenshot 2025-11-15 020902.png",
-            "tags": ["custom", "outfit"],
-        },
-
-        {
-            "name": "Purple Reign WWIC Short",
-            "category": "bottoms",
-            "image_url": "/static/images/Screenshot 2025-11-15 020917.png",
-            "tags": ["custom", "outfit"],
-        },
-
-        {
-            "name": "WWIC V2 SHORT",
-            "category": "bottoms",
-            "image_url": "/static/images/Screenshot 2025-11-15 020926.png",
-            "tags": ["custom", "outfit"],
-        },
-
-        {
-            "name": "WWIC V1 SHORT",
-            "category": "bottoms",
-            "image_url": "/static/images/Screenshot 2025-11-15 020931.png",
-            "tags": ["custom", "outfit"],
-        },
-
-        {
-            "name": "The One Short",
-            "category": "bottoms",
-            "image_url": "/static/images/Screenshot 2025-11-15 020937.png",
-            "tags": ["shorts", "outfit"],
-        },
-
-        {
-            "name": "WWIC V1 LONG SLEEVE",
-            "category": "tops",
-            "image_url": "/static/images/Screenshot 2025-11-15 020951.png",
-            "tags": ["custom", "outfit"],
-        },
-
-        {
-            "name": "WHY WOULD I CAP",
-            "category": "accessories",
-            "image_url": "/static/images/Screenshot 2025-11-15 021033.png",
-            "tags": ["hat", "rain"],
-        },
     ]
 
     return render_template(
-        "browse.html",
+        'browse.html',
         items=items,
-        item_count=len(items)
+        item_count=len(items),
+        form=form
     )
 
 
